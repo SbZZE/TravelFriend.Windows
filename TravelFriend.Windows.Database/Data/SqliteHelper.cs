58eff690622d5efdfb64a7d06f112a98873d3761
LinXiaoYi <874183200@qq.com>
2020-05-15 00:57:51 +0800

    [ADD]添加sqlite作为本地数据库

---
+++ TravelFriend.Windows.Database/Data/SqliteHelper.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TravelFriend.Windows.Database.Model;
+
+namespace TravelFriend.Windows.Database.Data
+{
+    public class SqliteHelper
+    {
+        public SqliteHelper()
+        {
+            var db = new SQLiteConnection("travelfriend.db");
+            db.CreateTable<User>();
+        }
+    }
+}
---
+++ TravelFriend.Windows.Database/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelFriend.Windows.Database.Model
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string NickName { get; set; }
+        public byte[] Avatar { get; set; }
+    }
+}
